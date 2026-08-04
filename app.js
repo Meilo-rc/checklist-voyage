db09f37b27667330e6bff1de8936d9a56588e9b7
Meilo-rc <psyco_set@hotmail.com>
2026-08-04 17:00:13 +0200

    Float add buttons bottom right

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const VERSION = "2.00";
+const VERSION = "2.01";
 const STORAGE_KEY = "checklist-voyage-state-v2";
 const OLD_STORAGE_KEY = "travelChecklistState";
 const PB_URL = "https://psyco.fly.dev";
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "checklist-voyage-2.00";
+const CACHE_NAME = "checklist-voyage-2.01";
 const APP_SHELL = [
   "./",
   "./index.html",
