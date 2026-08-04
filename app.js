9e7a12b94d7c65f0d13ff1017fefcfd8829a86de
Meilo-rc <psyco_set@hotmail.com>
2026-08-04 18:29:12 +0200

    Center imported category chips

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const VERSION = "2.13";
+const VERSION = "2.14";
 const STORAGE_KEY = "checklist-voyage-state-v2";
 const OLD_STORAGE_KEY = "travelChecklistState";
 const PB_URL = "https://psyco.fly.dev";
